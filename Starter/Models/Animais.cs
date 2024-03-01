bf722188a1ee04cba1269c38c806bfca6e786102
Matheus Fagundes <124789617+m4Fagundes@users.noreply.github.com>
2024-03-01 18:04:39 -0300

    Add files via upload

---
+++ Starter/Models/Animais.cs
@@ -0,0 +1,21 @@
+﻿public class Animais
+{
+    public string Species { get; set; }
+    public string ID { get; set; }
+    public string Age { get; set; }
+    public string PhysicalDescription { get; set; }
+    public string PersonalityDescription { get; set; }
+    public string Nickname { get; set; }
+    public decimal SuggestedDonation { get; set; }
+
+    public Animais(string species, string id, string age, string physicalDescription, string personalityDescription, string nickname, decimal suggestedDonation)
+    {
+        Species = species;
+        ID = id;
+        Age = age;
+        PhysicalDescription = physicalDescription;
+        PersonalityDescription = personalityDescription;
+        Nickname = nickname;
+        SuggestedDonation = suggestedDonation;
+    }
+}
